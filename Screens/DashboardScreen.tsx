cac9a310de8189045a2c879230e61cf7b1a7046f
Sayandip Adhikary <sayandipadhikary@Sayandips-MacBook-Air.local>
2026-05-11 16:36:35 +0530

    updated app

--- Screens/DashboardScreen.tsx
+++ Screens/DashboardScreen.tsx
@@ -1,3 +1,4 @@
+import { authenticateWithBiometrics } from '@/utils/biometric';
 import { Ionicons } from '@expo/vector-icons';
 import { useFocusEffect, useRouter } from 'expo-router';
 import React, { useRef, useState } from 'react';
@@ -412,8 +413,8 @@ export default function DashboardScreen() {
     };
 
     const onViewPress = async (item: Secret) => {
-        // const authenticated = await authenticateWithBiometrics('Authenticate to view secret');
-        // if (!authenticated) return;
+        const authenticated = await authenticateWithBiometrics('Authenticate to view secret');
+        if (!authenticated) return;
         router.push({
             pathname: '/(view)' as any,
             params: { id: item.id, name: item.name, secret: item.secret, description: item.description },
